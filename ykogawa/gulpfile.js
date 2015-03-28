840bb1689ef95134a54c8c96b4e12cb0dc7b621f
Y.Kogawa <kogawa@respect-pal.jp>
2015-03-28 17:37:47 +0900

    画像の圧縮機能追加 gulp build でパブリッシュフォルダを削除して再ビルド

--- ykogawa/gulpfile.js
+++ ykogawa/gulpfile.js
@@ -7,7 +7,7 @@ var del = require('del');
 
 var setting = {
   autoprefixer: {
-      browser: ['last 3 version', 'ie >= 8', 'Android 4.0']
+      browser: ['last 2 version', 'ie 9', 'ie 8', 'Android 4.0', 'Android 2.3']
   },
   sass: {
     option: {
@@ -19,10 +19,6 @@ var setting = {
       src: 'src',
       dest: 'httpdocs'
     },
-    clean: [
-      'httpdocs/assets/sass/',
-      'httpdocs/sftp-config.json'
-    ],
     sass: {
       src: 'src/assets/sass/**/*.scss',
       dest: 'httpdocs/assets/css/',
@@ -35,24 +31,32 @@ var setting = {
       src: 'src/assets/img/**/*',
       dest: 'httpdocs/assets/img/'
     },
+    js: {
+      src: 'src/assets/js/**/*',
+      dest: 'httpdocs/assets/js/'
+    },
     html: {
       src: ['src/**/*.html', 'src/**/*.php'],
       dest: 'httpdocs/',
+    },
+    etc: {
+      src: 'src/assets/etc/**/*',
+      dest: 'httpdocs/assets/etc/',
     },
   }
 }
 
 // 画像の圧縮
 gulp.task('imagemin', function(){
-  var srcGlob = setting.path.image.src;
-  var dstGlob = setting.path.image.dest;
   var imageminOptions = {
     optimizationLevel: 7
   };
 
-  gulp.src(srcGlob)
+  gulp.src(setting.path.image.src)
+    .pipe($.changed(setting.path.image.dest))
     .pipe($.imagemin(imageminOptions))
-    .pipe(gulp.dest(dstGlob));
+    .pipe(gulp.dest(setting.path.image.dest))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 // SASS
@@ -62,7 +66,6 @@ gulp.task('scss',function(){
       errorHandler: $.notify.onError("Error: <%= error.message %>") //<-
     }))
     .pipe($.sass(setting.sass.option))
-    .pipe($.changed(setting.path.sass.dest))
     .pipe($.autoprefixer(setting.autoprefixer.browser))
     .pipe(gulp.dest(setting.path.sass.dest))
     .pipe(browserSync.reload({stream: true}));
@@ -79,13 +82,37 @@ gulp.task('html', function(){
   .pipe(browserSync.reload({stream: true}));
 });
 
+// JavaScript
+gulp.task('js', function(){
+  return gulp.src(
+    setting.path.js.src,
+    {base: setting.path.base.src}
+  )
+  .pipe($.changed(setting.path.base.dest))
+  .pipe(gulp.dest(setting.path.base.dest))
+  .pipe(browserSync.reload({stream: true}));
+});
+
+// Etc
+gulp.task('etc', function(){
+  return gulp.src(
+    setting.path.etc.src
+  )
+  .pipe($.changed(setting.path.etc.dest))
+  .pipe(gulp.dest(setting.path.etc.dest))
+  .pipe(browserSync.reload({stream: true}));
+});
+
 // Clean
-gulp.task('clean', function(){
-  del(setting.path.clean);
+gulp.task('clean', del.bind(null, setting.path.base.dest));
+
+// Build
+gulp.task('build', function(){
+  return runSequence('clean', ['html', 'js', 'scss', 'etc'], 'imagemin');
 });
 
 // Watch
-gulp.task('watch',function(){
+gulp.task('watch', function(){
   browserSync.init({
     server:{
       baseDir: [setting.path.base.dest],
@@ -94,6 +121,7 @@ gulp.task('watch',function(){
 
   gulp.watch([setting.path.html.src], ['html']);
   gulp.watch([setting.path.sass.src], ['scss']);
+  gulp.watch([setting.path.image.src], ['imagemin']);
 });
 
 gulp.task('default',['watch']);
